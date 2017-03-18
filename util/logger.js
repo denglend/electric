605377b69a0847b97dca378ee3b951d417e47b16
dan <denglend@gmail.com>
2017-03-18 19:50:24 -0400

    Center-shorten long log messages

--- util/logger.js
+++ util/logger.js
@@ -14,14 +14,14 @@ let LogLevels = [
 	"silly",
 	"todo"
 ];
-let MaxMeta = 28;
+let MaxMeta = 36;
 exports.level = 'warn';
 
 for (let i of LogLevels) {
 	exports[i] = function(iv) {
 		return function(msg,meta) {
 			if (meta === undefined) meta = "";
-			let PaddedMeta = meta.length < MaxMeta ? meta + ' '.repeat(MaxMeta - meta.length) : meta.substring(0,MaxMeta);
+			let PaddedMeta = meta.length < MaxMeta ? meta + ' '.repeat(MaxMeta - meta.length) : meta.substr(0,MaxMeta/2-1)+'...'+meta.substr(-MaxMeta/2+2);
 			if (LogLevels.indexOf(iv) <= LogLevels.indexOf(exports.level)) {
 				console.log(LogFormats[LogLevels.indexOf(iv)]+" ["+PaddedMeta+"] - "+msg);
 			}
